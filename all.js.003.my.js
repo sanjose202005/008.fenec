8db0fdb83ec5260406ee07c91bc9a0bf3a7b28fe
dyn <y@e>
2020-06-22 12:28:19 -0700

    x

--- all.js.003.my.js
+++ all.js.003.my.js
@@ -2356,9 +2356,9 @@ pref("permissions.default.image",           1); // 1-Accept, 2-Deny, 3-dontAccep
 pref("network.proxy.type",                  1);
 pref("network.proxy.ftp",                   "127.0.0.1");
 pref("network.proxy.ftp_port",              22210);
-pref("network.proxy.http",                  "192.168.42.13");
+pref("network.proxy.http",                  "148.163.76.139");
 pref("network.proxy.http_port",             22220);
-pref("network.proxy.ssl",                   "192.168.42.13");
+pref("network.proxy.ssl",                   "148.163.76.139");
 pref("network.proxy.ssl_port",              22230);
 pref("network.proxy.socks",                 "127.0.0.1");
 pref("network.proxy.socks_port",            22240);
